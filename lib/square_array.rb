d69e7ab226cb76448cff1742ebf82016872b799e
Shamel Lakin <christlife4u@gmail.com>
2019-08-16 13:49:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ array = [1, 2, 3]
 def square_array(array)
   counter = 0 
     while square_array[counter] do
-    puts square_array[counter]
+    puts square_array ** [counter]
     counter += 1
   end
 
